26608c2e1e35422ca194074d8060d79f61c30e10
John May <johnmay@ebi.ac.uk>
2011-10-11 12:53:37 +0000

    Renamed package dialog.table to dialog.popup
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/mnb/trunk@581 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ mnb-dialog/src/main/java/uk/ac/ebi/mnb/dialog/popup/CrossReferenceEditor.java
@@ -0,0 +1,183 @@
+/**
+ * CrossReferenceEditor.java
+ *
+ * 2011.10.07
+ *
+ * This file is part of the CheMet library
+ * 
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.mnb.dialog.popup;
+
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.Sizes;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.apache.log4j.Logger;
+import uk.ac.ebi.annotation.crossreference.CrossReference;
+import uk.ac.ebi.interfaces.Identifier;
+import uk.ac.ebi.metabonater.components.theme.MRoundButton;
+import uk.ac.ebi.metabonater.components.theme.MTextField;
+import uk.ac.ebi.mnb.interfaces.Theme;
+import uk.ac.ebi.mnb.settings.Settings;
+import uk.ac.ebi.mnb.view.MComboBox;
+import uk.ac.ebi.resource.IdentifierFactory;
+
+/**
+ * @name    CrossReferenceEditor - 2011.10.07 <br>
+ *          Class description
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ */
+public class CrossReferenceEditor extends PopupDialog {
+
+    private static final Logger LOGGER = Logger.getLogger(CrossReferenceEditor.class);
+    private CellConstraints cc = new CellConstraints();
+    private Map<String, Byte> map = new HashMap();
+    private FormLayout layout;
+    private List<MComboBox> comboboxes = new LinkedList();
+    private List<MTextField> fields = new LinkedList();
+    private JPanel panel;
+
+    public CrossReferenceEditor(JFrame frame) {
+        super(frame);
+        for (Identifier id : IdentifierFactory.getInstance().getSupportedIdentifiers()) {
+            map.put(id.getShortDescription(), id.getIndex());
+        }
+        panel = getPanel();
+    }
+
+    public void setup(Collection<? extends CrossReference> references) {
+
+
+
+        layout = new FormLayout("min, p, min, min ", "");
+        panel.setLayout(layout);
+        comboboxes = new LinkedList();
+        fields = new LinkedList();
+
+
+        if (references.isEmpty()) {
+            layout.appendRow(new RowSpec(Sizes.PREFERRED));
+            MComboBox box = new MComboBox(map.keySet());
+            MTextField field = new MTextField(12);
+            fields.add(field);
+            comboboxes.add(box);
+        } else {
+            for (CrossReference reference : references) {
+                layout.appendRow(new RowSpec(Sizes.PREFERRED));
+                MComboBox box = new MComboBox(map.keySet());
+                box.setSelectedItem(reference.getIdentifier().getShortDescription());
+                MTextField field = new MTextField(reference.getIdentifier().getAccession(), 12);
+                fields.add(field);
+                comboboxes.add(box);
+            }
+        }
+
+        update();
+
+    }
+
+    private void update() {
+        panel.removeAll();
+        Theme theme = Settings.getInstance().getTheme();
+        for (int i = 0; i < fields.size(); i++) {
+            JButton minus = new MRoundButton(theme.getMinusIcon(), new RemoveCrossReference(this, i));
+            JButton plus = new MRoundButton(theme.getPlusIcon(), new AddCrossReference(this, i));
+            minus.setEnabled(!(fields.size() == 1));
+            panel.add(comboboxes.get(i), cc.xy(1, i + 1));
+            panel.add(fields.get(i), cc.xy(2, i + 1));
+            panel.add(minus, cc.xy(3, i + 1));
+            panel.add(plus, cc.xy(4, i + 1));
+        }
+
+    }
+
+    public Collection<CrossReference> getCrossReferences() {
+        Collection<CrossReference> xref = new ArrayList<CrossReference>();
+
+        for (int i = 0; i < fields.size(); i++) {
+            MComboBox box = comboboxes.get(i);
+            MTextField field = fields.get(i);
+            String accession = field.getText().trim();
+            if (box.getSelectedIndex() != 0 && accession.isEmpty() == false) {
+                String desc = (String) box.getSelectedItem();
+                Identifier id = IdentifierFactory.getInstance().ofIndex(map.get(desc));
+                id.setAccession(accession);
+                xref.add(new CrossReference(id));
+            }
+        }
+        return xref;
+    }
+
+    private class RemoveCrossReference extends AbstractAction {
+
+        private int index;
+        private JDialog dialog;
+
+        public RemoveCrossReference() {
+        }
+
+        public RemoveCrossReference(JDialog dialog, int index) {
+            this.index = index;
+            this.dialog = dialog;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            panel.removeAll();
+            layout.removeRow(layout.getRowCount()); // going to repaint all regardless
+            comboboxes.remove(index);
+            fields.remove(index);
+            update();
+            dialog.pack();
+        }
+    }
+
+    private class AddCrossReference extends AbstractAction {
+
+        private int index;
+        private JDialog dialog;
+
+        public AddCrossReference() {
+        }
+
+        public AddCrossReference(JDialog dialog, int index) {
+            this.index = index;
+            this.dialog = dialog;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            layout.appendRow(new RowSpec(Sizes.PREFERRED));
+            MComboBox box = new MComboBox(map.keySet());
+            MTextField field = new MTextField(12);
+            comboboxes.add(index + 1, box); // append after
+            fields.add(index + 1, field);
+            update();
+            dialog.pack();
+        }
+    }
+}
---
+++ mnb-dialog/src/main/java/uk/ac/ebi/mnb/dialog/popup/PopupDialog.java
@@ -0,0 +1,181 @@
+/**
+ * PopupDialog.java
+ *
+ * 2011.10.07
+ *
+ * This file is part of the CheMet library
+ * 
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.mnb.dialog.popup;
+
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.geom.Area;
+import java.awt.geom.RoundRectangle2D;
+
+import uk.ac.ebi.metabonater.components.theme.MRoundButton;
+import uk.ac.ebi.mnb.core.CloseDialogAction;
+import uk.ac.ebi.mnb.interfaces.Theme;
+import uk.ac.ebi.mnb.settings.Settings;
+import uk.ac.ebi.mnb.view.DialogPanel;
+import uk.ac.ebi.mnb.view.ViewUtils;
+
+import javax.swing.*;
+
+import org.apache.log4j.Logger;
+
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+import com.sun.awt.AWTUtilities;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import uk.ac.ebi.visualisation.ColorUtilities;
+
+/**
+ * @name    PopupDialog - 2011.10.07 <br>
+ *          Class description
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ */
+public class PopupDialog extends JDialog {
+
+    private static final Logger LOGGER = Logger.getLogger(PopupDialog.class);
+    private JPanel panel = new DialogPanel();
+    private BufferedImage bgImage;
+    private Point mouse;
+    private JPanel background = new JPanel() {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(bgImage, 0, 0, null);
+        }
+    };
+
+    public PopupDialog(JFrame frame) {
+        super(frame, ModalityType.APPLICATION_MODAL);
+        //  setUndecorated(true);
+        setUndecorated(true);
+        add(background);
+        AWTUtilities.setWindowOpaque(this, false);
+        panel.setOpaque(false);
+        background.setLayout(new FormLayout("8px, 16px, p, 16px, 8px", "8px, 16px, p, 16px, 8px"));
+        CellConstraints cc = new CellConstraints();
+        Icon close = ViewUtils.getIcon("images/cutout/close_whitebg_16x16.png");
+        JButton closeButton = new MRoundButton(close, new CloseDialogAction(this, false));
+        background.add(closeButton, cc.xy(2, 2));
+        background.setOpaque(true);
+        background.add(panel, cc.xy(3, 3));
+        addComponentListener(new ComponentAdapter() {
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+                bgImage = getBackgroundImage();
+            }
+
+            @Override
+            public void componentResized(ComponentEvent e) {
+                setLocation(mouse.x - (getWidth() / 2), mouse.y - getHeight() + 7);
+                bgImage = getBackgroundImage();
+                repaint();
+            }
+        });
+    }
+
+    /**
+     * Sets the pop-up location based on mouse position. The tip of the callout will be at the mouse point
+     */
+    public void setOnMouse() {
+        mouse = MouseInfo.getPointerInfo().getLocation();
+        setLocation(mouse.x - (getWidth() / 2), mouse.y - getHeight() + 7);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+    private Map<Dimension, BufferedImage> backgroundCache = new HashMap();
+
+    public BufferedImage getBackgroundImage() {
+
+        Theme theme = Settings.getInstance().getTheme();
+        Dimension size = getPreferredSize();
+
+        if (backgroundCache.containsKey(size)) {
+            return backgroundCache.get(size);
+        }
+
+        if (backgroundCache.size() > 200) {
+            LOGGER.warn("Many images are being stored the background caching map has now be reinstantiated" + " considering making your popup discrete sizes prevent constant background re-drawing");
+            backgroundCache = new HashMap();
+        }
+
+        LOGGER.info("Drawing new background image for size: " + size);
+
+        BufferedImage img = new BufferedImage(size.width, size.height, BufferedImage.TYPE_4BYTE_ABGR);
+
+        Graphics2D g2 = img.createGraphics();
+//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+//                            RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        Shape callout = getCalloutShape();
+        // draws the border
+        int sw = 10 * 2;
+        Color grey = new Color(0, 0, 0, 50);
+        for (int i = sw; i >= 2; i -= 2) {
+            float pct = (float) (sw - i) / (sw - 1);
+            g2.setColor(ColorUtilities.getMixedColor(grey, pct,
+                                                     ViewUtils.CLEAR_COLOUR, 1.0f - pct));
+            g2.setStroke(new BasicStroke(i));
+            g2.draw(callout);
+        }
+
+        g2.setColor(theme.getDialogBackground());
+        g2.fill(callout);
+        g2.dispose();
+
+        backgroundCache.put(size, img);
+
+        return img;
+    }
+
+    public Area getCalloutShape() {
+
+        RoundRectangle2D rect = new RoundRectangle2D.Float(16, 16, getPreferredSize().width - 32, getPreferredSize().height - 32, 16, 16);
+
+        // 16 for corners as icon is 16 rounded
+
+        int center = getPreferredSize().width / 2;
+
+        int[] x = new int[]{
+            center,
+            center - 25,
+            center + 25
+        };
+        int[] y = new int[]{
+            getPreferredSize().height - 7,
+            getPreferredSize().height - 32,
+            getPreferredSize().height - 32};
+        Polygon p = new Polygon(x, y, 3);
+
+        Area composite = new Area(rect);
+        composite.add(new Area(p));
+
+        return composite;
+
+    }
+}
