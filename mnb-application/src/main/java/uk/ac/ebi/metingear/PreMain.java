bcef2364906db9818f98d8be593c2392c376e518
John May <johnmay@ebi.ac.uk>
2012-03-20 10:43:59 +0000

    Changed how the application is launched
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/mnb/trunk@1828 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ mnb-application/src/main/java/uk/ac/ebi/metingear/PreMain.java
@@ -0,0 +1,52 @@
+package uk.ac.ebi.metingear;
+
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+/**
+ * PreMain - 13.03.2012 <br/>
+ * <p/>
+ * Class descriptions.
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class PreMain {
+
+    private static final Logger LOGGER = Logger.getLogger(PreMain.class);
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        final String javaBin    = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+
+        /* Build command: java -jar application.jar */
+        final ArrayList<String> command = new ArrayList<String>();
+        command.add(javaBin);
+        for (String jvmArg : ManagementFactory.getRuntimeMXBean().getInputArguments()) {
+            // command.add(jvmArg);
+        }
+        command.add("-cp");
+        command.add(ManagementFactory.getRuntimeMXBean().getClassPath());
+        command.add(Main.class.getName());
+        for (String arg : args) {
+            command.add(arg);
+        }
+
+        final ProcessBuilder builder = new ProcessBuilder(command);
+
+
+        System.out.println(command);
+        builder.start();
+        System.exit(0);
+
+    }
+    
+
+
+}
