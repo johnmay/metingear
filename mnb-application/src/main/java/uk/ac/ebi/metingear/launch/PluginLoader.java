1469a635ac241f53424761715568c9bfd294678e
John May <john.wilkinsonmay@gmail.com>
2012-04-23 16:58:47 +0100

    Added DialogLauncherFactory and PluginLoader
    These are what strap the plugin's into the menus

---
+++ mnb-application/src/main/java/uk/ac/ebi/metingear/launch/PluginLoader.java
@@ -0,0 +1,90 @@
+package uk.ac.ebi.metingear.launch;
+
+import uk.ac.ebi.metingear.view.PlugableDialog;
+import uk.ac.ebi.mnb.main.MainView;
+import uk.ac.ebi.mnb.menu.ContextMenu;
+import uk.ac.ebi.mnb.menu.MainMenuBar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * @version $Rev$
+ */
+public class PluginLoader {
+
+
+    private ServiceLoader<PlugableDialog> loader;
+    private MainView view;
+    private MainMenuBar menu;
+    private DialogLauncherFactory factory;
+
+    public PluginLoader(MainView view, MainMenuBar menu) {
+        this.view = view;
+        this.menu = menu;
+        this.loader = ServiceLoader.load(PlugableDialog.class);
+        this.factory = new DialogLauncherFactory(view,
+                                                 view,
+                                                 view.getViewController(),
+                                                 view.getUndoManager(),
+                                                 view.getMessageManager());
+    }
+
+    public void load() {
+        Iterator<PlugableDialog> plugin = loader.iterator();
+        while (plugin.hasNext()) {
+            load(plugin.next());
+        }
+    }
+
+    public void load(PlugableDialog plugin) {
+
+        JMenu menu = getMenu(plugin.getMenuPath().iterator());
+
+        if (menu instanceof ContextMenu) {
+            ((ContextMenu) menu).add(factory.getLauncher(plugin.getDialogClass()), plugin.getContext());
+        } else {
+            menu.add(factory.getLauncher(plugin.getDialogClass()));
+        }
+
+    }
+
+    public JMenu getMenu(Iterator<String> path) {
+
+        JComponent menu = this.menu;
+        while (path.hasNext()) {
+            menu = getMenu(menu, path.next());
+        }
+
+        return (JMenu) menu;
+
+    }
+
+    public JMenu getMenu(JComponent root, String query) {
+
+        for (Component component : root.getComponents()) {
+            if (component instanceof JMenu) {
+                String subject = ((JMenu) component).getText();
+                if (subject.equalsIgnoreCase(query)) {
+                    return (JMenu) component;
+                }
+            }
+        }
+
+        ContextMenu menu = new ContextMenu(query, view);
+
+
+
+        if(root instanceof ContextMenu){
+            ((ContextMenu)root).add(menu);
+        } else {
+            root.add(menu);
+        }
+
+        return menu;
+
+    }
+
+}
