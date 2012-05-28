66824aa429838be547cf6402540f1cfb72317909
John May <john.wilkinsonmay@gmail.com>
2012-05-28 09:14:17 +0100

    Made sure we 'startup' the service

--- action/src/main/java/uk/ac/ebi/mnb/dialog/tools/TransferAnnotations.java
+++ action/src/main/java/uk/ac/ebi/mnb/dialog/tools/TransferAnnotations.java
@@ -4,17 +4,17 @@
  * 2011.10.13
  *
  * This file is part of the CheMet library
- * 
+ *
  * The CheMet library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * CheMet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,32 +23,32 @@ package uk.ac.ebi.mnb.dialog.tools;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import org.apache.log4j.Logger;
-import uk.ac.ebi.mdk.domain.annotation.crossreference.CrossReference;
 import uk.ac.ebi.caf.report.ReportManager;
+import uk.ac.ebi.mdk.domain.DefaultIdentifierFactory;
+import uk.ac.ebi.mdk.domain.annotation.crossreference.CrossReference;
+import uk.ac.ebi.mdk.domain.entity.GeneProduct;
+import uk.ac.ebi.mdk.domain.entity.ProteinProductImpl;
+import uk.ac.ebi.mdk.domain.identifier.Identifier;
 import uk.ac.ebi.mdk.domain.identifier.IdentifierSet;
 import uk.ac.ebi.mdk.domain.identifier.SwissProtIdentifier;
-import uk.ac.ebi.mdk.service.query.crossreference.UniProtCrossReferenceService;
-import uk.ac.ebi.mdk.domain.entity.ProteinProductImpl;
-import uk.ac.ebi.mdk.domain.entity.GeneProduct;
-import uk.ac.ebi.mdk.domain.identifier.Identifier;
 import uk.ac.ebi.mdk.domain.observation.Observation;
+import uk.ac.ebi.mdk.domain.observation.sequence.LocalAlignment;
 import uk.ac.ebi.mdk.service.query.CrossReferenceService;
+import uk.ac.ebi.mdk.service.query.crossreference.UniProtCrossReferenceService;
 import uk.ac.ebi.mnb.core.ControllerDialog;
 import uk.ac.ebi.mnb.interfaces.SelectionController;
 import uk.ac.ebi.mnb.interfaces.TargetedUpdate;
-import uk.ac.ebi.mdk.domain.observation.sequence.LocalAlignment;
-import uk.ac.ebi.mdk.domain.DefaultIdentifierFactory;
 
 import javax.swing.*;
 import javax.swing.event.UndoableEditListener;
 import java.util.Collection;
 
 /**
- * @name    Annotate - 2011.10.13 <br>
- *          Class description
+ * @author johnmay
+ * @author $Author$ (this version)
  * @version $Rev$ : Last Changed $Date$
- * @author  johnmay
- * @author  $Author$ (this version)
+ * @name Annotate - 2011.10.13 <br>
+ * Class description
  */
 public class TransferAnnotations
         extends ControllerDialog {
@@ -68,6 +68,7 @@ public class TransferAnnotations
     public void process() {
 
         CrossReferenceService<SwissProtIdentifier> service = new UniProtCrossReferenceService();
+        service.startup();
 
 
         DefaultIdentifierFactory factory = DefaultIdentifierFactory.getInstance();
@@ -81,7 +82,7 @@ public class TransferAnnotations
                 LocalAlignment alignment = (LocalAlignment) observation;
                 IdentifierSet set = factory.resolveSequenceHeader(alignment.getSubject());
                 for (Identifier identifier : set.getSubIdentifiers(SwissProtIdentifier.class)) {
-                    for(Identifier xref : service.getCrossReferences((SwissProtIdentifier)identifier)){
+                    for (Identifier xref : service.getCrossReferences((SwissProtIdentifier) identifier)) {
                         identifiers.put(xref, observation);
                     }
                 }
