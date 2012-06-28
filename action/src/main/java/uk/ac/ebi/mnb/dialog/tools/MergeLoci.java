6f184e5618b242de7e4fc11a74bf3baa29dbe735
John May <john.wilkinsonmay@gmail.com>
2012-06-28 11:49:04 -0400

    Updated copyright info (LGPL v3)

--- action/src/main/java/uk/ac/ebi/mnb/dialog/tools/MergeLoci.java
+++ action/src/main/java/uk/ac/ebi/mnb/dialog/tools/MergeLoci.java
@@ -1,22 +1,18 @@
-/**
- * MergeLoci.java
+/*
+ * Copyright (c) 2012. John May <jwmay@sf.net>
  *
- * 2011.10.13
- *
- * This file is part of the CheMet library
- * 
- * The CheMet library is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * CheMet is distributed in the hope that it will be useful,
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.mnb.dialog.tools;
 
