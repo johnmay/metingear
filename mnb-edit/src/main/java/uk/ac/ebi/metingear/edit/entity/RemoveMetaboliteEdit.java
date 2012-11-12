9551b324fdbad2a39f8dcb20398d7c07f139aed5
John May <john.wilkinsonmay@gmail.com>
2012-11-12 13:42:58 +0000

    Added an edit which allows us to remove metabolites from a reconstruction

---
+++ mnb-edit/src/main/java/uk/ac/ebi/metingear/edit/entity/RemoveMetaboliteEdit.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2012. John May <jwmay@users.sf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ebi.metingear.edit.entity;
+
+import uk.ac.ebi.mdk.domain.entity.Metabolite;
+import uk.ac.ebi.mdk.domain.entity.Reconstruction;
+import uk.ac.ebi.mdk.domain.entity.collection.Reactome;
+import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReaction;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.CompoundEdit;
+import java.util.Collection;
+
+/**
+ * An undoable edit that removes a metabolite from a reconstruction.
+ *
+ * @author John May
+ */
+public class RemoveMetaboliteEdit extends CompoundEdit {
+
+    private Reconstruction reconstruction;
+    private Metabolite metabolite;
+    private final Collection<MetabolicReaction> reactions;
+
+    /**
+     * An undoable edit which removes a metabolite from the provide
+     * reconstruction. This edit will also redo/undo the presence of a
+     * metabolite in various reactions.
+     *
+     * @param reconstruction a reconstruction containing the metabolite
+     * @param metabolite     a metabolite in the reaction
+     */
+    public RemoveMetaboliteEdit(final Reconstruction reconstruction,
+                                final Metabolite metabolite) {
+
+        final Reactome reactome = reconstruction.getReactome();
+        this.reactions = reconstruction.getReactome()
+                                       .getReactions(metabolite);
+
+        // remove from metabolome
+        super.addEdit(new AbstractUndoableEdit() {
+            @Override
+            public void undo() throws CannotUndoException {
+                reconstruction.getMetabolome().add(metabolite);
+            }
+
+            @Override
+            public void redo() throws CannotRedoException {
+                reconstruction.getMetabolome().remove(metabolite);
+            }
+        });
+
+        // remove references from reactome
+        for (final MetabolicReaction reaction : reactions) {
+            super.addEdit(new AbstractUndoableEdit() {
+                @Override
+                public void undo() throws CannotUndoException {
+                    reactome.update(reaction);
+                }
+
+                @Override
+                public void redo() throws CannotRedoException {
+                    reactome.removeKey(metabolite, reaction);
+                }
+            });
+            super.addEdit(new RemoveParticipantEdit(metabolite, reaction));
+        }
+
+        super.end();
+
+    }
+
+
+}
---
+++ mnb-edit/src/test/java/uk/ac/ebi/metingear/edit/entity/RemoveMetaboliteEditTest.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2012. John May <jwmay@users.sf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ebi.metingear.edit.entity;
+
+import org.junit.Test;
+import uk.ac.ebi.mdk.domain.entity.MetaboliteImpl;
+import uk.ac.ebi.mdk.domain.entity.Reconstruction;
+import uk.ac.ebi.mdk.domain.entity.ReconstructionImpl;
+import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicParticipantImplementation;
+import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReaction;
+import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReactionImpl;
+
+import javax.swing.undo.UndoableEdit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author John May
+ */
+public class RemoveMetaboliteEditTest {
+
+    @Test
+    public void testUndo() {
+
+        MetaboliteImpl a = new MetaboliteImpl("a", "a");
+        MetaboliteImpl b = new MetaboliteImpl("b", "b");
+        MetaboliteImpl c = new MetaboliteImpl("c", "c");
+        MetaboliteImpl d = new MetaboliteImpl("d", "d");
+
+        MetabolicReaction r1 = new MetabolicReactionImpl();
+        MetabolicReaction r2 = new MetabolicReactionImpl();
+
+        r1.addReactant(new MetabolicParticipantImplementation(a));
+        r1.addReactant(new MetabolicParticipantImplementation(b));
+        r1.addProduct(new MetabolicParticipantImplementation(c));
+
+        r2.addReactant(new MetabolicParticipantImplementation(c));
+        r2.addReactant(new MetabolicParticipantImplementation(d));
+        r2.addProduct(new MetabolicParticipantImplementation(a));
+
+        Reconstruction reconstruction = new ReconstructionImpl();
+
+        reconstruction.addMetabolite(a);
+        reconstruction.addMetabolite(b);
+        reconstruction.addMetabolite(c);
+        reconstruction.addMetabolite(d);
+        reconstruction.getReactome().add(r1);
+        reconstruction.getReactome().add(r2);
+
+        UndoableEdit edit = new RemoveMetaboliteEdit(reconstruction, a);
+
+        // remove metabolite 'a'
+        reconstruction.getMetabolome().remove(a);
+        reconstruction.getReactome().removeKey(a, r1);
+        reconstruction.getReactome().removeKey(a, r2);
+        r1.removeReactant(r1.getReactants().get(0));
+        r2.removeProduct(r2.getProducts().get(0));
+
+        assertFalse("metabolome contained metabolite, ",
+                    reconstruction.getMetabolome().contains(a));
+        assertEquals("metabolome contained metabolite, ",
+                     0,
+                     reconstruction.getReactome().getReactions(a).size());
+        assertEquals("incorrect r1 reactant count, ",
+                     1,
+                     r1.getReactantCount());
+        assertEquals("incorrect r2 product count, ",
+                     0,
+                     r2.getProductCount());
+
+        // undo the removal
+        edit.undo();
+
+        assertTrue("metabolome did not contain metabolite, ",
+                   reconstruction.getMetabolome().contains(a));
+        assertEquals("metabolome contained metabolite, ",
+                     2,
+                     reconstruction.getReactome().getReactions(a).size());
+        assertEquals("incorrect r1 reactant count, ",
+                     2,
+                     r1.getReactantCount());
+        assertEquals("incorrect r2 product count, ",
+                     1,
+                     r2.getProductCount());
+
+    }
+
+    @Test
+    public void testRedo() {
+
+        MetaboliteImpl a = new MetaboliteImpl("a", "a");
+        MetaboliteImpl b = new MetaboliteImpl("b", "b");
+        MetaboliteImpl c = new MetaboliteImpl("c", "c");
+        MetaboliteImpl d = new MetaboliteImpl("d", "d");
+
+        MetabolicReaction r1 = new MetabolicReactionImpl();
+        MetabolicReaction r2 = new MetabolicReactionImpl();
+
+        r1.addReactant(new MetabolicParticipantImplementation(a));
+        r1.addReactant(new MetabolicParticipantImplementation(b));
+        r1.addProduct(new MetabolicParticipantImplementation(c));
+
+        r2.addReactant(new MetabolicParticipantImplementation(c));
+        r2.addReactant(new MetabolicParticipantImplementation(d));
+        r2.addProduct(new MetabolicParticipantImplementation(a));
+
+        Reconstruction reconstruction = new ReconstructionImpl();
+
+        reconstruction.addMetabolite(a);
+        reconstruction.addMetabolite(b);
+        reconstruction.addMetabolite(c);
+        reconstruction.addMetabolite(d);
+        reconstruction.getReactome().add(r1);
+        reconstruction.getReactome().add(r2);
+
+        UndoableEdit edit = new RemoveMetaboliteEdit(reconstruction, a);
+
+        // remove metabolite 'a'
+        reconstruction.getMetabolome().remove(a);
+        reconstruction.getReactome().removeKey(a, r1);
+        reconstruction.getReactome().removeKey(a, r2);
+        r1.removeReactant(r1.getReactants().get(0));
+        r2.removeProduct(r2.getProducts().get(0));
+
+        assertFalse("metabolome contained metabolite, ",
+                    reconstruction.getMetabolome().contains(a));
+        assertEquals("metabolome contained metabolite, ",
+                     0,
+                     reconstruction.getReactome().getReactions(a).size());
+        assertEquals("incorrect r1 reactant count, ",
+                     1,
+                     r1.getReactantCount());
+        assertEquals("incorrect r2 product count, ",
+                     0,
+                     r2.getProductCount());
+
+        // undo the removal
+        edit.undo();
+
+        assertTrue("metabolome did not contain metabolite, ",
+                   reconstruction.getMetabolome().contains(a));
+        assertEquals("metabolome contained metabolite, ",
+                     2,
+                     reconstruction.getReactome().getReactions(a).size());
+        assertEquals("incorrect r1 reactant count, ",
+                     2,
+                     r1.getReactantCount());
+        assertEquals("incorrect r2 product count, ",
+                     1,
+                     r2.getProductCount());
+
+        // redo the removal
+        edit.redo();
+
+        assertFalse("metabolome contained metabolite, ",
+                    reconstruction.getMetabolome().contains(a));
+        assertEquals("metabolome contained metabolite, ",
+                     0,
+                     reconstruction.getReactome().getReactions(a).size());
+        assertEquals("incorrect r1 reactant count, ",
+                     1,
+                     r1.getReactantCount());
+        assertEquals("incorrect r2 product count, ",
+                     0,
+                     r2.getProductCount());
+
+    }
+
+
+}
