9f86d455e8fb8ec9d49211df0b531db583387534
John May <john.wilkinsonmay@gmail.com>
2013-01-23 12:50:09 +0000

    Updated copyright on metingear-simulate module

--- optimise/src/main/java/uk/ac/ebi/optimise/gap/GapFind.java
+++ optimise/src/main/java/uk/ac/ebi/optimise/gap/GapFind.java
@@ -1,24 +1,18 @@
 /*
- *  DeadEndDetector.java
+ * Copyright (c) 2013. John May <jwmay@users.sf.net>
  *
- *  CheMet Version 1.0
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  2011.07.11
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  This file is part of the ChemMet library
- *
- *  The ChemMet library is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  ChemMet is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with ChemMet.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.optimise.gap;
 
