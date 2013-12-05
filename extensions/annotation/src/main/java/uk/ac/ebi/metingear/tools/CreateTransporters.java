0c0554f0cb7355cf70ef0fa11cab7fb3609503df
John May <john.wilkinsonmay@gmail.com>
2013-12-05 11:50:22 +0000

    Clean up transporter dialog layout.

--- extensions/annotation/src/main/java/uk/ac/ebi/metingear/tools/CreateTransporters.java
+++ extensions/annotation/src/main/java/uk/ac/ebi/metingear/tools/CreateTransporters.java
@@ -23,15 +23,23 @@ import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.layout.Sizes;
 import uk.ac.ebi.caf.component.factory.CheckBoxFactory;
 import uk.ac.ebi.caf.component.factory.LabelFactory;
+import uk.ac.ebi.caf.component.factory.PanelFactory;
 import uk.ac.ebi.mdk.domain.entity.AnnotatedEntity;
 import uk.ac.ebi.mdk.domain.entity.Reconstruction;
 import uk.ac.ebi.mdk.domain.entity.collection.DefaultReconstructionManager;
 import uk.ac.ebi.mdk.domain.entity.collection.ReconstructionManager;
 import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReaction;
 import uk.ac.ebi.mdk.domain.entity.reaction.compartment.Organelle;
+import uk.ac.ebi.mdk.tool.transport.AminoAcid;
+import uk.ac.ebi.mdk.tool.transport.LibraryStructure;
+import uk.ac.ebi.mdk.tool.transport.Misc;
+import uk.ac.ebi.mdk.tool.transport.Nucleo;
+import uk.ac.ebi.mdk.tool.transport.Saccharides;
 import uk.ac.ebi.mdk.tool.transport.TransportFactory;
 import uk.ac.ebi.metingear.edit.entity.AddEntitiesEdit;
 import uk.ac.ebi.metingear.view.AbstractControlDialog;
+import uk.ac.ebi.mnb.core.ErrorMessage;
+import uk.ac.ebi.mnb.core.ExpandableComponentGroup;
 
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
@@ -42,7 +50,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static uk.ac.ebi.mdk.tool.transport.TransportFactory.AminoAcid;
 
 /**
  * Dialog for specifying transport reactions.
@@ -51,7 +58,8 @@ import static uk.ac.ebi.mdk.tool.transport.TransportFactory.AminoAcid;
  */
 public final class CreateTransporters extends AbstractControlDialog {
 
-    Map<JCheckBox, AminoAcid> aaSymport = new HashMap<JCheckBox, AminoAcid>();
+    Map<JCheckBox, LibraryStructure> symportCompounds = new HashMap<JCheckBox, LibraryStructure>();
+    Map<JCheckBox, LibraryStructure> abcCompounds    = new HashMap<JCheckBox, LibraryStructure>();
 
     public CreateTransporters(Window window) {
         super(window);
@@ -60,46 +68,140 @@ public final class CreateTransporters extends AbstractControlDialog {
     @Override public JComponent createForm() {
         JComponent component = super.createForm();
 
-        FormLayout layout = new FormLayout("p, 4dlu, p", "p");
+        FormLayout layout = new FormLayout("left:p",
+                                           "p, 4dlu, p");
         component.setLayout(layout);
 
         CellConstraints cc = new CellConstraints();
 
-        component.add(getLabel("aa.trans"), cc.xy(1, 1));
-
-        AminoAcid[] aas = AminoAcid.values();
-
-        for (AminoAcid aa : aas) {
-            layout.appendRow(new RowSpec(Sizes.PREFERRED));
-            JCheckBox cb = CheckBoxFactory.newCheckBox();
-            component.add(LabelFactory.newFormLabel(aa.name()), cc.xy(1, layout.getRowCount()));
-            component.add(cb, cc.xy(3, layout.getRowCount()));
-            aaSymport.put(cb, aa);
+        JComponent protonSymport = PanelFactory.create();
+        {
+            FormLayout symportLayout = new FormLayout("p, 4dlu, p, 4dlu, p");
+            protonSymport.setLayout(symportLayout);
+            symportLayout.appendRow(new RowSpec(Sizes.DLUX14));
+            protonSymport.add(LabelFactory.newLabel("Amino acids"), cc.xyw(1, symportLayout.getRowCount(), 5));
+            AminoAcid[] aas = AminoAcid.values();
+            Nucleo[]    ncs = Nucleo.values();
+            
+            int aaIdx = 0;
+            while (aaIdx < aas.length) {
+                symportLayout.appendRow(new RowSpec(Sizes.PREFERRED));
+                for (int x = 0; aaIdx < aas.length && x < 3; x++) {
+                    AminoAcid aa = aas[aaIdx++];
+                    JCheckBox cb = CheckBoxFactory.newCheckBox(aa.structureName());
+                    protonSymport.add(cb, cc.xy(2 * x + 1, symportLayout.getRowCount())); 
+                    symportCompounds.put(cb, aa);
+                }
+            }
+            
+            symportLayout.appendRow(new RowSpec(Sizes.DLUX14));                
+            protonSymport.add(LabelFactory.newLabel("Nucleobases / Nucleosides"), cc.xyw(1, symportLayout.getRowCount(), 5));
+            int ncIdx = 0;
+            while (ncIdx < ncs.length) {
+                symportLayout.appendRow(new RowSpec(Sizes.PREFERRED));
+                for (int x = 0; ncIdx < ncs.length && x < 3; x++) {
+                    Nucleo nucleo = ncs[ncIdx++];
+                    JCheckBox cb = CheckBoxFactory.newCheckBox(nucleo.structureName());
+                    protonSymport.add(cb, cc.xy(2 * x + 1, symportLayout.getRowCount()));
+                    symportCompounds.put(cb, nucleo);
+                }
+            }
         }
+
+        component.add(new ExpandableComponentGroup("Proton Symport",
+                                                   protonSymport,
+                                                   this), cc.xy(1, 1));
+
+        JComponent abc = PanelFactory.create();
+        {
+
+            AminoAcid[]   aas   = AminoAcid.values();
+            Saccharides[] schs  = Saccharides.values();
+            Misc[]        mscs = Misc.values();
+            
+            FormLayout abcLayout = new FormLayout("p, 4dlu, p, 4dlu, p");
+            abc.setLayout(abcLayout);
+            abcLayout.appendRow(new RowSpec(Sizes.DLUX14));
+            abc.add(LabelFactory.newLabel("Amino acids"), cc.xyw(1, abcLayout.getRowCount(), 5));
+            int aaIdx = 0;
+            while (aaIdx < aas.length) {
+                abcLayout.appendRow(new RowSpec(Sizes.PREFERRED));
+                for (int x = 0; aaIdx < aas.length && x < 3; x++) {
+                    AminoAcid aa = aas[aaIdx++];
+                    JCheckBox cb = CheckBoxFactory.newCheckBox(aa.structureName());
+                    abc.add(cb, cc.xy(2 * x + 1, abcLayout.getRowCount()));
+                    abcCompounds.put(cb, aa);
+                }
+            }
+            abcLayout.appendRow(new RowSpec(Sizes.DLUX14));
+            abc.add(LabelFactory.newLabel("Saccharides"), cc.xyw(1, abcLayout.getRowCount(), 5));
+
+            int schIdx = 0;
+            while (schIdx < schs.length) {
+                abcLayout.appendRow(new RowSpec(Sizes.PREFERRED));
+                for (int x = 0; schIdx < schs.length && x < 3; x++) {
+                    Saccharides sch = schs[schIdx++];
+                    JCheckBox cb = CheckBoxFactory.newCheckBox(sch.structureName());
+                    abc.add(cb, cc.xy(2 * x + 1, abcLayout.getRowCount()));
+                    abcCompounds.put(cb, sch);
+                }
+            }
+
+            abcLayout.appendRow(new RowSpec(Sizes.DLUX14));
+            abc.add(LabelFactory.newLabel("Misc (Minerals, Ions, etc.)"), cc.xyw(1, abcLayout.getRowCount(), 5));
+
+            int mscIdx = 0;
+            while (mscIdx < mscs.length) {
+                abcLayout.appendRow(new RowSpec(Sizes.PREFERRED));
+                for (int x = 0; mscIdx < mscs.length && x < 3; x++) {
+                    Misc msc = mscs[mscIdx++];
+                    JCheckBox cb = CheckBoxFactory.newCheckBox(msc.structureName());
+                    abc.add(cb, cc.xy(2 * x + 1, abcLayout.getRowCount()));
+                    abcCompounds.put(cb, msc);
+                }
+            }
+        }
+
+        component.add(new ExpandableComponentGroup("ATP Binding Casettee (ABC) Transporters",
+                                                   abc,
+                                                   this), cc.xy(1, 3));
 
         return component;
     }
 
     @Override public void process() {
+        try {
+            List<LibraryStructure> symportStructures = new ArrayList<LibraryStructure>();
+            List<LibraryStructure> abcStructures = new ArrayList<LibraryStructure>();
 
-        List<AminoAcid> aas = new ArrayList<AminoAcid>();
-
-        for (Map.Entry<JCheckBox, AminoAcid> e : aaSymport.entrySet()) {
-            if (e.getKey().isSelected()) {
-                aas.add(e.getValue());
+            for (Map.Entry<JCheckBox, LibraryStructure> e : symportCompounds.entrySet()) {
+                if (e.getKey().isSelected()) {
+                    symportStructures.add(e.getValue());
+                }
             }
+
+            for (Map.Entry<JCheckBox, LibraryStructure> e : abcCompounds.entrySet()) {
+                if (e.getKey().isSelected()) {
+                    abcStructures.add(e.getValue());
+                }
+            }
+
+            TransportFactory tf = new TransportFactory(getEntityFactory());
+            Collection<AnnotatedEntity> reactions = new ArrayList<AnnotatedEntity>();
+
+            for (MetabolicReaction mr : tf.protonSymport(symportStructures, Organelle.CYTOPLASM, Organelle.EXTRACELLULAR))
+                reactions.add(mr);
+            for (MetabolicReaction mr : tf.abc(abcStructures, Organelle.EXTRACELLULAR, Organelle.CYTOPLASM))
+                reactions.add(mr);
+
+            ReconstructionManager reconstructionManager = DefaultReconstructionManager.getInstance();
+            Reconstruction reconstruction = reconstructionManager.active();
+
+            AddEntitiesEdit edit = new AddEntitiesEdit(reconstruction, reactions);
+            addEdit(edit);
+            edit.apply();
+        } catch (Exception e) {
+            addReport(new ErrorMessage("Internal error: " + e.getMessage() + " cause: " + e.getCause()));
         }
-
-        TransportFactory tf = new TransportFactory(getEntityFactory());
-        Collection<AnnotatedEntity> reactions = new ArrayList<AnnotatedEntity>();
-        for (MetabolicReaction mr : tf.aaProtonSymporter(aas, Organelle.CYTOPLASM, Organelle.EXTRACELLULAR))
-            reactions.add(mr);
-
-        ReconstructionManager reconstructionManager = DefaultReconstructionManager.getInstance();
-        Reconstruction reconstruction = reconstructionManager.active();
-
-        AddEntitiesEdit edit = new AddEntitiesEdit(reconstruction, reactions);
-        addEdit(edit);
-        edit.apply();
     }
 }
